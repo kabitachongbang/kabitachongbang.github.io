f3cce4a78dde2e985d9230c9f48e9174474600b5
Kabita Chongbang <kabitachongbang786@gmail.com>
2019-03-30 14:11:02 +0800

    created scss file

---
+++ app/route.js
@@ -0,0 +1,26 @@
+angular.module('myPortfolio', ['ngRoute']);
+
+angular.module('myPortfolio').config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'app/view/portfolio.html'
+        })
+        .when('/about', {
+            templateUrl: 'app/view/about.html'
+        })
+        .when('/contact', {
+            templateUrl: 'app/view/contact.html'
+        })
+        .when('/projects', {
+            templateUrl: 'app/view/projects.html',
+        })
+        .when('/education', {
+            templateUrl: 'app/view/education.html'
+        })
+
+
+
+
+
+
+});
