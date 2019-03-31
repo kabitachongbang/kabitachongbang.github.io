69d40f98c8eede7db3afc64d5983c32db27ab597
Kabita Chongbang <kabitachongbang786@gmail.com>
2019-03-31 13:22:02 +0800

    added js file

---
+++ app/controller/index.js
@@ -0,0 +1,10 @@
+(function(){
+    'use strict';
+    angular.module('myPortfolio',[]);
+    angular.module('myPortfolio').controller['MainController'];
+
+    MainController.$inject=['$scope'];
+    function MainController ($scope){
+        $inputSearchData='';
+    }
+})();
