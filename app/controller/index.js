fdb6b427f65c7471d3ac303fb30d835ae888a6db
Kabita Chongbang <kabitachongbang786@gmail.com>
2019-03-31 14:53:45 +0800

    saved all changes

--- app/controller/index.js
+++ app/controller/index.js
@@ -1,10 +1,18 @@
-(function(){
+(function () {
     'use strict';
-    angular.module('myPortfolio',[]);
-    angular.module('myPortfolio').controller['MainController'];
+    angular.module('myPortfolio')
+        .controller('MainController', MainController);
 
-    MainController.$inject=['$scope'];
-    function MainController ($scope){
-        $inputSearchData='';
+    MainController.$inject = ['$scope', '$location'];
+    function MainController($scope, $location) {
+        $scope.inputSearch = 'inputSearch';
+        $scope.hireMe = function () {
+            console.log('hello here');
+            $location.path('/contact');
+        };
+        $scope.showCV = function () {
+            console.log('CV here');
+            $location.path('/projects');
+        }
     }
 })();
